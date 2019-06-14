a7bb8d3b194ab1728d3a2b9ef9064459707248c0
627134613@qq.com <627134613@qq.com>
2019-06-14 20:40:58 +0800

    firstCommit

---
+++ app/build/generated/source/atlascore/debug/android/taobao/atlas/framework/FrameworkProperties.java
@@ -0,0 +1,8 @@
+package android.taobao.atlas.framework;
+public class FrameworkProperties {
+private String version = "1.0.0";
+public String getVersion() {return version;}
+public static String bundleInfo = "[{\"activities\":[\"com.taobao.firstbundle.FirstBundleActivity\"],\"applicationName\":\"com.taobao.firstbundle.FirstBundleApplication\",\"contentProviders\":[],\"dependency\":[],\"isInternal\":true,\"pkgName\":\"com.taobao.firstbundle\",\"receivers\":[],\"services\":[],\"unique_tag\":\"5df4bc7cbea3895833e3db3a3015111e\",\"version\":\"1.0.0@1.0.0\"}]";
+public static String autoStartBundles = "com.taobao.firstbundle";
+public static String outApp = "false";
+}
